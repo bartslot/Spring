e2075fe02b2e69c8ddeec561d125e4cf9d9d2b92
bartslot <bartslot@gmail.com>
2018-08-12 12:26:04 +0200

    V1.1
    Added Particle systems

---
+++ Assets/_Scripts/effectPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class effectPlayer : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int y = 0; y < 5; y++)
+        {
+            for (int x = 0; x < 5; x++)
+            {
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.AddComponent<Rigidbody>();
+                cube.transform.position = new Vector3(x, y, 0);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/_Scripts/onBeat.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class onBeat : MonoBehaviour {
+    public int beatSection;
+
+    public int spawnPositionCubes = 0;
+
+    public AudioMixer _audioMixer;
+    // Use this for initialization
+    void Start () {
+        beatSection = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (BeatPeer._beatCountX8 == 0 && BeatPeer._beatFull)
+        {
+               Debug.Log("8e beat #" + beatSection.ToString());
+                beatSection++;
+            _audioMixer.SetFloat("lowcutoff", Random.Range(500f, 10000f));
+          //  _audioMixer.SetFloat("delayecho", Random.Range(500f, 10000f));
+
+        }
+        if (beatSection == 2)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cube.AddComponent<Rigidbody>();
+            cube.transform.position = new Vector3(spawnPositionCubes, spawnPositionCubes, 0);
+        }
+
+        if (BeatPeer._beatFull)
+        {
+            Debug.Log("er is een beat nu");
+        }
+
+        
+	}
+}
